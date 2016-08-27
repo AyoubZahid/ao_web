9575bc38436ecd92c1e2b57eb213acf2237a9883
AyoubZahid <azahid@agilorg.com>
2016-08-27 07:39:14 -0400

    [IMP] integration de la library MetroUI

--- static/src/js/main.js
+++ static/src/js/main.js
@@ -3,6 +3,7 @@ openerp.ao_web = function(instance) {
     instance.ao_web = {};
     var module = instance.ao_web;
 
-    openerp_aoweb_widgets(instance,module);  
+    openerp_aoweb_widgets(instance,module);
+    openerp_aoweb_screens(instance,module);   
 
 };
--- static/src/js/widgets.js
+++ static/src/js/widgets.js
@@ -15,88 +15,16 @@ function openerp_aoweb_widgets(instance, module) {
         },
     });
     
-    //**** popups ****
-    module.PopUpWidget = module.BaseWidget.extend({
-        show: function(){
-            if(this.$el){
-                this.$el.removeClass('oe_hidden');
-            }
-        },
-        /* called before hide, when a popup is closed */
-        close: function(){
-        },
-        /* hides the popup. keep in mind that this is called in the initialization pass of the 
-         * pos instantiation, so you don't want to do anything fancy in here */
-        hide: function(){
-            if(this.$el){
-                this.$el.addClass('oe_hidden');
-            }
-        },
-    });
-
-    module.ErrorPopupWidget = module.PopUpWidget.extend({
-        template:'ErrorPopup',
-        show: function(options){
-            options = options || {};
-            var self = this;
-            this._super();
-
-            $('body').append('<audio src="/ao_web/static/src/sounds/error.wav" autoplay="true"></audio>');
-
-            this.message = options.message || _t('Error');
-            this.comment = options.comment || '';
-
-            this.renderElement();
-
-            this.$('.footer .button').click(function(){
-                //self.pos_widget.screen_selector.close_popup();
-                if ( options.confirm ) {
-                    options.confirm.call(self);
-                }
-            });
-        },
-    });
-
-    module.ErrorTracebackPopupWidget = module.ErrorPopupWidget.extend({
-        template:'ErrorTracebackPopup',
-    });
-
-    module.ConfirmPopupWidget = module.PopUpWidget.extend({
-        template: 'ConfirmPopup',
-        show: function(options){
-            var self = this;
-            this._super();
-
-            this.message = options.message || '';
-            this.comment = options.comment || '';
-            this.renderElement();
-            
-            this.$('.button.cancel').click(function(){
-                self.pos_widget.screen_selector.close_popup();
-                if( options.cancel ){
-                    options.cancel.call(self);
-                }
-            });
-
-            this.$('.button.confirm').click(function(){
-                self.pos_widget.screen_selector.close_popup();
-                if( options.confirm ){
-                    options.confirm.call(self);
-                }
-            });
-        },
-    });
-
-    
     /***** button widget ****/
     module.ButtonWidget = module.BaseWidget.extend({
-        template: 'Button',
+        template: 'AOButton',
         init: function(parent, options){
             options = options || {};
             this._super(parent, options);
             this.action = options.action;
             this.label  = options.label;
             this.icon   = options.icon;
+            this.rightalign = options.rightalign || false;
         },
         renderElement: function(){
             var self = this;
@@ -116,4 +44,89 @@ function openerp_aoweb_widgets(instance, module) {
         },
     });    
 
+	/***** action bar widget *****/
+    module.ActionBarWidget = instance.web.Widget.extend({
+        template:'AOActionBarWidget',
+        init: function(parent, options){
+            this._super(parent,options);
+            this.button_list = [];
+            this.buttons = {};
+            this.visibility = {};
+        },
+        set_element_visible: function(element, visible, action){
+            if(visible != this.visibility[element]){
+                this.visibility[element] = !!visible;
+                if(visible){
+                    this.$('.'+element).removeClass('oe_hidden');
+                }else{
+                    this.$('.'+element).addClass('oe_hidden');
+                }
+            }
+            if(visible && action){
+                this.action[element] = action;
+                this.$('.'+element).off('click').click(action);
+            }
+        },
+        set_button_disabled: function(name, disabled){
+            var b = this.buttons[name];
+            if(b){
+                b.set_disabled(disabled);
+            }
+        },
+        destroy_buttons:function(){
+            for(var i = 0; i < this.button_list.length; i++){
+                this.button_list[i].destroy();
+            }
+            this.button_list = [];
+            this.buttons = {};
+            return this;
+        },
+        get_button_count: function(){
+            return this.button_list.length;
+        },
+        add_new_button: function(button_options){
+            var button = new module.ActionButtonWidget(this,button_options);
+            this.button_list.push(button);
+            if(button_options.name){
+                this.buttons[button_options.name] = button;
+            }
+            button.appendTo(this.$('.ao-actionbar-button-list'));
+            return button;
+        },
+        show:function(){
+        	//showMetroCharm(this.$el,'bottom-charm');
+            this.$el.removeClass('oe_hidden');
+        },
+        hide:function(){
+        	//hideMetroCharm(this.$el);
+            this.$el.addClass('oe_hidden');
+        },
+    });
+    module.ActionButtonWidget = instance.web.Widget.extend({
+        template:'AOActionButtonWidget',
+        icon_template:'AOActionButtonWidgetWithIcon',
+        init: function(parent, options){
+            this._super(parent, options);
+            this.label = options.label || 'button';
+            this.rightalign = options.rightalign || false;
+            this.click_action = options.click;
+            this.disabled = options.disabled || false;
+            if(options.icon){
+                this.icon = options.icon;
+                this.template = this.icon_template;
+            }
+        },
+        set_disabled: function(disabled){
+            if(this.disabled != disabled){
+                this.disabled = !!disabled;
+                this.renderElement();
+            }
+        },
+        renderElement: function(){
+            this._super();
+            if(this.click_action && !this.disabled){
+                this.$el.click(_.bind(this.click_action, this));
+            }
+        },
+    });
 }; 
